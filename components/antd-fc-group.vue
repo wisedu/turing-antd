4a671f43194d61eb9871c790f48fd675babebea0
戚雨 <yuqi@wisedu.com>
2018-07-18 14:21:41 +0800

    更新group组件样式

--- components/antd-fc-group.vue
+++ components/antd-fc-group.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h4>{{name}}</h4>
+        <h4 class="antd-fc-group-title">{{name}}</h4>
         <label>{{desc}}</label>
         <slot></slot>
     </div>
@@ -17,5 +17,13 @@ export default {
 </script>
 
 <style>
-
+.antd-fc-group-title {
+    font-size: 16px;
+    font-weight: 700;
+    border-left: 4px solid #3E50B4;
+    padding-left: 8px;
+    line-height: 16px;
+    min-height: 0;
+    margin-bottom: 16px
+}
 </style>
