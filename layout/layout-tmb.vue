040b37092c4206cfa1a26740feae6769fea0b331
戚雨 <yuqi@wisedu.com>
2018-07-20 19:47:03 +0800

    增加上中下布局组件

--- layout/layout-tmb.vue
+++ layout/layout-tmb.vue
@@ -2,27 +2,41 @@
     <Layout>
         <Header>
             <slot name="menu">
-                <Menu mode="horizontal" theme="dark" active-name="1">
+                <Menu mode="horizontal" theme="dark" :active-name="activeName" @on-select="menuSelect">
                     <div class="layout-logo">
-                        <slot name="logo"></slot>
+                        <slot name="logo">
+                            <img class="logo" :src="logo">
+                        </slot>
+                    </div>
+                    <div class="user-dropdown-menu-con">
+                        <Dropdown transfer trigger="click" @on-click="handleClickUserDropdown">
+                            <a href="javascript:void(0)">
+                                <span class="main-user-name"> {{userName}} </span>
+                                <Icon type="arrow-down-b"></Icon>
+                            </a>
+                            <DropdownMenu slot="list">
+                                <DropdownItem :name="item.name" v-for="item in dropMenu" :key="item.name" >{{item.name}}</DropdownItem>
+                            </DropdownMenu>
+                        </Dropdown>
+                        <img :src="userImage" class="avatar">
                     </div>
                     <div class="layout-nav">
-                        <menuItem name="1">
-                            <Icon type="ios-navigate"></Icon>
-                            Item 1
-                        </menuItem>
-                        <menuItem name="2">
-                            <Icon type="ios-keypad"></Icon>
-                            Item 2
-                        </menuItem>
-                        <menuItem name="3">
-                            <Icon type="ios-analytics"></Icon>
-                            Item 3
-                        </menuItem>
-                        <menuItem name="4">
-                            <Icon type="ios-paper"></Icon>
-                            Item 4
-                        </menuItem>
+                        <template v-for="item in menu">
+                            <menuItem :name="item.name" v-if="item.items === undefined" :key="item.name">
+                                <Icon :type="item.icon" v-if="item.icon !== undefined"></Icon>
+                                {{item.name}}
+                            </menuItem>
+                            <Submenu name="3" v-if="item.items !== undefined" :key="item.name">
+                                <template slot="title">
+                                    <Icon :type="item.icon" v-if="item.icon !== undefined"></Icon>
+                                    {{item.name}}
+                                </template>
+                                <MenuItem :name="subitem.name" v-for="subitem in item.items" :key="subitem.name">
+                                    <Icon :type="subitem.icon" v-if="subitem.icon !== undefined"></Icon>
+                                    {{subitem.name}}
+                                </MenuItem>
+                            </Submenu>
+                        </template>
                     </div>
                 </Menu>
             </slot>
@@ -49,8 +63,45 @@
 export default {
     name: "layout-tmb",
     props: {
-        menu: Object,
-        navPath: Object
+        menu: {
+            type: Array,
+            default: function() {
+                return [];
+            }
+        },
+        navPath: Object,
+        dropMenu: Array,
+        logo: String,
+        userImage: String,
+        userName: String,
+        userInfo: {
+            type:Object,
+            default: function() {
+                return {};
+            }
+        },
+        activeName:String
+    },
+    methods:{
+        handleClickUserDropdown (name) {
+            let clickitem = this.dropMenu.find(item => item.name === name);
+            if (clickitem !== undefined){ 
+                if (clickitem.url !== undefined) {
+                    window.location.href = clickitem.url;
+                }
+                if (clickitem.callback !== undefined) {
+                    clickitem.callback();
+                }
+            }
+        },
+        menuSelect(name) {
+            let clickitem = this.menu.find(item => item.name === name);
+            if (clickitem !== undefined){ 
+                if (clickitem.url !== undefined && clickitem.url !== "") {
+                    window.location.href = clickitem.url;
+                }
+            }
+        }
     }
 };
 </script>
@@ -64,21 +115,37 @@ export default {
     overflow: hidden;
 }
 .layout-logo{
-    width: 100px;
-    height: 30px;
-    background: #5b6270;
-    border-radius: 3px;
     float: left;
-    position: relative;
-    top: 15px;
-    left: 20px;
+}
+.layout-logo img.logo{
+    height: 48px;
 }
 .layout-nav{
-    width: 420px;
+    float: right;
     margin: 0 auto;
     margin-right: 20px;
 }
 .layout-footer-center{
     text-align: center;
+}
+.user-dropdown-menu-con{
+    width: 180px;
+    float: right;
+    text-align: right;
+}
+.user-dropdown-menu-con .main-user-name{
+    display: inline-block;
+    width: 80px;
+    font-size: 14px;
+    word-break: keep-all;
+    white-space: nowrap;
+    vertical-align: middle;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    text-align: right;
+}
+.user-dropdown-menu-con .avatar{
+    margin-left: 10px;
+    text-align: right;
 }
 </style>
--- src/app.vue
+++ src/app.vue
@@ -1,11 +1,61 @@
 <template>
-    <div class="layout">
-        <router-view></router-view>
-    </div>
+    <layout-tmb v-bind="header">
+        <div slot="content">
+            <router-view></router-view>
+        </div>
+        <div slot="footer">
+            金智教育
+        </div>
+    </layout-tmb>
 </template>
 <script>
+import layoutTmb from '../layout/layout-tmb.vue';
 export default {
-    
+    data(){
+        return {
+            header:{
+                activeName:"B",
+                logo:"http://my.wisedu.com/new/portal/custom/img/logo/logo-mini.png",
+                userImage:"http://my.wisedu.com/portal/img/icon/user-role-teacher.png",
+                userName:"qiyu",
+                menu:[{
+                    name:"A",
+                    icon:"ios-people",
+                    url:"http://www.baidu.com"
+                },{
+                    name:"B",
+                    url:"#/table"
+                },{
+                    name:"C",
+                    url:""
+                },{
+                    name:"D",
+                    url:"",
+                    icon:"stats-bars",
+                    items:[{
+                        name:"DD",
+                        icon:"settings",
+                        url:""
+                    }]
+                }],
+                dropMenu:[{
+                    name:"个人中心",
+                    callback: function() {
+                        alert("个人中心");
+                    }
+                },{
+                    name:"退出",
+                    url:"http://www.baidu.com",
+                    callback: function() {
+                        alert("退出");
+                    }
+                }]
+            }
+        };
+    },
+    components:{
+        layoutTmb
+    }
 }
 </script>
 <style>
