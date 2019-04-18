0bbc3b98613aa5b4b6d932f06f87c9c8a1631cb8
xujiabin <xujiabin0114@163.com>
2019-04-18 14:27:20 +0800

    调整兼容下拉多选

--- components/antd-fc-select.vue
+++ components/antd-fc-select.vue
@@ -161,7 +161,7 @@ export default {
                     labels = undefined;
                 }
                 this.$emit("on-item-change", this.name, values, labels, this.model)
-                this.$emit("input", values)
+                this.$emit("input", values.join(','))
             } else {
                 let selected = item;
                 if (selected === undefined) {
